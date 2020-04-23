39bd14762c7ef00ef850a8fef0ae2244ca2bfb27
Hariharan Ramanan <harryharanr@gmail.com>
2020-04-23 15:11:06 +0530

    AppBar added

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./ui/header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      Hello!
+    </div>
+  );
+}
+
+export default App;
